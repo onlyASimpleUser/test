5b80fadec0cd2df9f5f878e8044d38cad6b3ef7e
onlyASi <60228794+onlyASimpleUser@users.noreply.github.com>
2023-12-02 13:12:55 -0500

    r

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+console.log("uhadsahdausdhasdkjankjdasnkdasjdkajdhksjd")
+var titles = [
+    "Now alive!",
+    "As a webpage!",
+    "Ongi etorri",
+    "Welcome",
+    "Bienvenido",
+    "Välkommen",
+    "THERE ISN'T NOTHING BETTER THAN BKHABSDSAHDNASKD",
+    "Simple User - Simple User - Simple User - Simple User - Simple User - Simple User - Simple User - Simple User - Simple User - Simple User",
+    "Let's play again!",
+    "syntax error unexpected ' ' (t_sl) expecting end of file"
+]
+
+document.title = "Simple User - " + titles[Math.floor(Math.random() * titles.length)]
